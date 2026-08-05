df280ee275e41f43ff3c4b7df147171187a708fc
Eliasse13 <eliasse.b@live.fr>
2026-08-05 11:25:08 +0200

    Add files via upload

--- js/facade/life.js
+++ js/facade/life.js
@@ -311,11 +311,3 @@ export {
   vieActive,
   voiture
 };
-
-/* ---- gestionnaires en attribut ---- */
-/* Ces fonctions sont appelées depuis des attributs onclick écrits
-   dans le HTML généré. Un module ES n'expose rien globalement :
-   on les rend accessibles explicitement. */
-Object.assign(window, {
-  ouvreLesPortes
-});
